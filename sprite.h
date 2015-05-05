06643c798e5ad8d293e8a8c7bcaed939022728ef
tpascua11 <tedlpascua@gmail.com>
2015-05-05 10:43:08 -0700

    Fixed texture bug when drawing another object

--- sprite.h
+++ sprite.h
@@ -99,7 +99,7 @@ void Sprite::setClip(int x, int y){
 void Sprite::initSprite(){
   //Get an image in input inside the texure
   image = ppm6GetImage(imageName);
-  glGenTextures(3, &texture);
+  glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
