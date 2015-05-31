55bd98e6066b8cad80019ea8b8506f6ae8d60944
Beth Armitage <bethgrace5@gmail.com>
2015-05-31 14:20:42 -0700

    demonstrtate getting type of object

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <typeinfo>
 #include <string>
 #include <sys/time.h>
 #include <unistd.h>
@@ -736,6 +737,7 @@ void deleteItem(int id) {
 void makeEnemy(int w, int h, Object *ground, int type) {
     if (enemies_length<MAX_ENEMIES){
         enemies[enemies_length] = new Enemy(w, h, ground, type); 
+        printf ("%s\n",typeid(enemies[enemies_length]).name());
         switch (type){
             case 1:
                 enemies[enemies_length]->insert("./images/enemy1.ppm", 26, 1);
